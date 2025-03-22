3e505c95a2c4b8d318393164536d871f67ab6ac1
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2025-03-22 23:35:31 +0200

    re-test-after-back-end deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000';
-const API_URL = 'https://react-mern-back-end.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
