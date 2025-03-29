2685e2ce33bc2932bc8e89324149aae73695d922
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2025-03-29 18:21:00 +0200

    finalized-payment and started UI/UX HCD Design wireframe for front-page

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Ensure the API URL doesn't have a trailing slash
 const api = axios.create({
